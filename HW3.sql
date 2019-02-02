c278022e47e4fef7d6a9f99af8a2d1df72abd743
yuliakor <47179106+yuliakor@users.noreply.github.com>
2019-02-02 16:48:17 +0300

    Create HW3.sql

---
+++ HW3.sql
@@ -0,0 +1,60 @@
+
+SELECT 'ФИО: Корнеева Юлия';
+
+--1.Простые выборки
+--    1.1
+SELECT *
+FROM public.ratings LIMIT 10;
+--    1.2
+ SELECT *
+ FROM public.links
+ WHERE
+ imdbId like '%42' AND
+ movieid >=100 AND movieid <= 1000;
+ 
+ 
+ -- 2.Сложные выборки: JOIN
+  --    2.1
+ SELECT imdbId
+ FROM public.links
+ INNER JOIN  public.ratings
+    ON links.movieid=ratings.movieid
+ WHERE ratings.rating= 5
+ LIMIT 10;
+  
+
+ -- 3. Аггрегация данных 
+   -- 3.1
+ SELECT COUNT(rating) as count
+ FROM public.ratings
+ WHERE ratings.rating is NULL;
+   -- 3.2
+ SELECT COUNT(userid) as countuserid
+ FROM public.ratings
+ HAVING AVG(rating) > 3.5
+ LIMIT 10;
+ 
+ 
+ --  4. Иерархические запросы
+   -- 4.1
+ SELECT *
+FROM
+   (
+   SELECT imdbId
+   FROM links JOIN ratings ON links.movieid=ratings.movieid
+   GROUP BY imdbId
+   HAVING AVG(rating) > 3.5
+) table1
+limit 10;
+
+  --4.2
+WITH tab1 AS (
+   SELECT userid,
+   AVG(rating) as average
+   FROM public.ratings
+   GROUP BY userid
+    HAVING COUNT(userid) > 10
+)
+  SELECT 
+  AVG(average)
+  FROM tab1;
